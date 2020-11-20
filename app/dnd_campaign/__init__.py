4a5d46babcc2fbb5bc2d40e07fbc7391998c9c9a
tassaron2 <rutachigal@gmail.com>
2020-11-20 09:46:08 -0500

    explain what dnd_campaign is

--- app/dnd_campaign/__init__.py
+++ app/dnd_campaign/__init__.py
@@ -1,2 +1,9 @@
+"""
+This subpackage of tabletop_story is similar to the dnd_character dependency,
+but for things that are outside the scope of D&D characters yet inside the scope of a D&D campaign.
+
+dnd_campaign is not big enough for its own repo yet, but it could be in the future.
+"""
+
 from .combat import Combat
 from .npc import NPC
