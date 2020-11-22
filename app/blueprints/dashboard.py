44a1ee40e273b636e14be86384776831cc696b4b
tassaron2 <rutachigal@gmail.com>
2020-11-22 16:22:14 -0500

    add campaigns to dashboard

--- app/blueprints/dashboard.py
+++ app/blueprints/dashboard.py
@@ -1,7 +1,7 @@
 from flask import *
 import flask_login
 from tabletop_story.plugins import bcrypt, db
-from tabletop_story.models import GameCharacter
+from tabletop_story.models import GameCharacter, GameCampaign
 from tabletop_story.blueprints import charimg
 
 
@@ -38,6 +38,7 @@ def full_alignment_title(two_letters):
 @blueprint.route("/")
 def index():
     characters = []
+    campaigns = []
     is_logged_in = flask_login.current_user.is_authenticated
     if is_logged_in:
         user_id = int(flask_login.current_user.get_id())
@@ -50,9 +51,24 @@ def index():
         for i, character in enumerate(characters):
             character.image = charimg.charimg(*list(db_characters[i].design.values()))
             character.dbid = db_characters[i].id
+        for character in db_characters:
+            campaigns_ = (
+                GameCampaign.query.filter_by(character1=character.id).first(),
+                GameCampaign.query.filter_by(character2=character.id).first(),
+                GameCampaign.query.filter_by(character3=character.id).first(),
+                GameCampaign.query.filter_by(character4=character.id).first(),
+                GameCampaign.query.filter_by(character5=character.id).first(),
+                GameCampaign.query.filter_by(character6=character.id).first(),
+            )
+            for campaign in campaigns_:
+                if campaign is not None:
+                    campaigns.append(campaign)
+        gm_campaigns = GameCampaign.query.filter_by(gamemaster=user_id).all()
+        campaigns.extend([] if gm_campaigns is None else gm_campaigns)
 
     return render_template(
         "dashboard.html",
         logged_in=is_logged_in,
         characters=characters,
+        campaigns=set(campaigns),
     )
